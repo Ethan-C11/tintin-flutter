9f00f4acf7a91ee5b19415ac89b5b468fbaf1e76
Ethan-C11 <ethan.collignon.1102@gmail.com>
2024-10-17 09:18:27 +0200

    étape 3

--- lib/services/album_service.dart
+++ lib/services/album_service.dart
@@ -1,7 +1,18 @@
+import 'dart:math';
 import 'package:tintin/models/album.dart';
+import 'package:tintin/models/gps.dart';
+import 'package:faker/faker.dart';
 
 class AlbumService {
   static  Future<List<album>> generateAlbums(int length) async {
-    return List<album>.generate(length, (int counter) => album.newAlbum(title, numero: counter, year, image, resume, gps, location));
+    String pays = faker.address.country();
+    return List<album>.generate(length, (int counter) => album(title: "Tintin  au $pays",
+        numero: counter,
+        year: faker.date.year as int,
+        image: faker.image.loremPicsum(),
+        resume: faker.lorem.sentence(),
+        gps: new GPS(Random().nextDouble()*100, Random().nextDouble()*100),
+        location: pays)
+    );
   }
 }
